8813aed0b28bdbd6716bae45b31571a5c43455dd
Adam Farden <adam@farden.cz>
2015-08-30 03:11:46 +0200

    Build with locales

--- full-build.sh
+++ full-build.sh
@@ -11,8 +11,14 @@ export B2G_SYSTEM_APPS=1 &&
 export B2G_UPDATE_CHANNEL=default &&
 export MOZ_TELEMETRY_REPORTING=1 &&
 export MOZ_CRASHREPORTER_NO_REPORT=1 &&
+# Gaia & Spark
 export GAIA_DISTRIBUTION_DIR=distros/spark &&
 export GAIA_OPTIMIZE=1 &&
+# Extra Locales
+export LOCALE_BASEDIR=$PWD/gaia/locales &&
+export LOCALES_FILE=$PWD/gaia/locales/languages_all.json &&
+export GAIA_DEFAULT_LOCALE=en-GB &&
+# Firefox OS Branding
 export MOZILLA_OFFICIAL=1 &&
 export ENABLE_DEFAULT_BOOTANIMATION=true &&
 ./build.sh
