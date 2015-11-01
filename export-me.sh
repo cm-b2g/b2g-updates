a6ac002374c45da46518f6589ba371ef334e5457
Adam Farden <adam@farden.cz>
2015-11-01 09:47:06 +0100

    add back Locales

--- export-me.sh
+++ export-me.sh
@@ -9,4 +9,7 @@ export MOZ_TELEMETRY_REPORTING=1 &&
 export MOZ_CRASHREPORTER_NO_REPORT=1 &&
 export GAIA_DISTRIBUTION_DIR=distros/spark &&
 export GAIA_OPTIMIZE=1 &&
+export LOCALE_BASEDIR=$PWD/gaia/locales &&
+export LOCALES_FILE=$PWD/gaia/locales/languages_all.json &&
+export GAIA_DEFAULT_LOCALE=en-GB &&
 export ENABLE_DEFAULT_BOOTANIMATION=true
